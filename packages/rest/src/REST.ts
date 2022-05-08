5cb9c470f9b8132c217ea288486701f86105d3da
Splatterxl <splatterxl@outlook.ie>
2022-05-08 12:29:00 +0100

    feat: extend rest methods
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- packages/rest/src/REST.ts
+++ packages/rest/src/REST.ts
@@ -1,23 +1,34 @@
-import { APIRequest } from './APIRequest.js';
+import {
+  RESTDeleteAPIChannelMessageResult,
+  RESTDeleteAPIChannelResult,
+  RESTGetAPIAuditLogQuery,
+  RESTGetAPIAuditLogResult,
+  RESTGetAPIChannelInvitesResult,
+  RESTGetAPIChannelMessageReactionUsersQuery,
+  RESTGetAPIChannelMessageReactionUsersResult,
+  RESTGetAPIChannelMessageResult,
+  RESTGetAPIChannelMessagesQuery,
+  RESTGetAPIChannelMessagesResult,
+  RESTGetAPIChannelResult,
+  RESTPatchAPIChannelJSONBody,
+  RESTPatchAPIChannelMessageJSONBody,
+  RESTPatchAPIChannelMessageResult,
+  RESTPatchAPIChannelResult,
+  RESTPostAPIChannelMessageCrosspostResult,
+  RESTPostAPIChannelMessageJSONBody,
+  RESTPostAPIChannelMessageResult,
+  RESTPostAPIChannelMessagesBulkDeleteResult,
+  RESTPutAPIChannelMessageReactionResult,
+  RESTPutAPIChannelPermissionJSONBody,
+  RESTPutAPIChannelPermissionResult,
+} from 'discord-api-types/v10';
+import { APIRequest, File } from './APIRequest.js';
 import {
   RequestManager,
   RequestManagerOptions,
   RouteLike,
 } from './RequestManager.js';
 import { RESTClient, RESTClientOptions } from './RESTClient.js';
-import {
-  RESTDeleteAPIChannelResult,
-  RESTGetAPIAuditLogQuery,
-  RESTGetAPIAuditLogResult,
-  RESTGetAPIChannelMessageResult,
-  RESTGetAPIChannelMessagesQuery,
-  RESTGetAPIChannelMessagesResult,
-  RESTGetAPIChannelResult,
-  RESTPatchAPIChannelJSONBody,
-  RESTPatchAPIChannelResult,
-  RESTPostAPIChannelMessageFormDataBody,
-  RESTPostAPIChannelMessageJSONBody,
-} from 'discord-api-types/v10';
 
 type RequestOptions = Partial<APIRequest>;
 
@@ -147,6 +158,162 @@ export class REST extends RequestManager {
   getChannelMessage(channelID: string, messageID: string) {
     return this.get<RESTGetAPIChannelMessageResult>(
       `/channels/${channelID}/messages/${messageID}`,
+    );
+  }
+
+  createMessage(
+    channelID: string,
+    data: RESTPostAPIChannelMessageJSONBody,
+    files?: File[],
+  ) {
+    return this.post<RESTPostAPIChannelMessageResult>(
+      `/channels/${channelID}/messages`,
+      {
+        body: data,
+        files,
+      },
+    );
+  }
+
+  crosspostMessage(channelID: string, messageID: string) {
+    return this.post<RESTPostAPIChannelMessageCrosspostResult>(
+      `/channels/${channelID}/messages/${messageID}/crosspost`,
+      {
+        body: '',
+      },
+    );
+  }
+
+  createReaction(channelID: string, messageID: string, emoji: string) {
+    return this.put<RESTPutAPIChannelMessageReactionResult>(
+      `/channels/${channelID}/messages/${messageID}/reactions/${encodeURIComponent(
+        emoji,
+      )}/@me`,
+      {},
+    );
+  }
+
+  deleteOwnReaction(channelID: string, messageID: string, emoji: string) {
+    return this.delete(
+      `/channels/${channelID}/messages/${messageID}/reactions/${encodeURIComponent(
+        emoji,
+      )}/@me`,
+    );
+  }
+
+  deleteUserReaction(
+    channelID: string,
+    messageID: string,
+    emoji: string,
+    userID: string,
+  ) {
+    return this.delete(
+      `/channels/${channelID}/messages/${messageID}/reactions/${encodeURIComponent(
+        emoji,
+      )}/${userID}`,
+    );
+  }
+
+  getReactions(
+    channelID: string,
+    messageID: string,
+    emoji: string,
+    options?: RESTGetAPIChannelMessageReactionUsersQuery,
+  ) {
+    return this.get<RESTGetAPIChannelMessageReactionUsersResult>(
+      `/channels/${channelID}/messages/${messageID}/reactions/${encodeURIComponent(
+        emoji,
+      )}`,
+      {
+        query: options,
+      },
+    );
+  }
+
+  deleteAllReactions(channelID: string, messageID: string) {
+    return this.delete(
+      `/channels/${channelID}/messages/${messageID}/reactions`,
+    );
+  }
+
+  deleteAllReactionsForEmoji(
+    channelID: string,
+    messageID: string,
+    emoji: string,
+  ) {
+    return this.delete(
+      `/channels/${channelID}/messages/${messageID}/reactions/${encodeURIComponent(
+        emoji,
+      )}`,
+    );
+  }
+
+  editMessage(
+    channelID: string,
+    messageID: string,
+    data: RESTPatchAPIChannelMessageJSONBody,
+    files?: File[],
+  ) {
+    return this.patch<RESTPatchAPIChannelMessageResult>(
+      `/channels/${channelID}/messages/${messageID}`,
+      {
+        body: data,
+        files,
+      },
+    );
+  }
+
+  deleteMessage(channelID: string, messageID: string, reason?: string) {
+    return this.delete<RESTDeleteAPIChannelMessageResult>(
+      `/channels/${channelID}/messages/${messageID}`,
+      {
+        reason,
+      },
+    );
+  }
+
+  async bulkDeleteMessages(
+    channelID: string,
+    messages: string[] | number,
+    reason?: string,
+  ) {
+    if (typeof messages === 'number') {
+      messages = await this.getChannelMessages(channelID, {
+        limit: messages,
+      }).then(res => res.map(m => m.id));
+    }
+
+    messages = messages as string[];
+
+    return this.post<RESTPostAPIChannelMessagesBulkDeleteResult>(
+      `/channels/${channelID}/messages/bulk-delete`,
+      {
+        body: {
+          messages,
+        },
+        reason,
+      },
+    );
+  }
+
+  editChannelPermissions(
+    channelID: string,
+    overwriteID: string,
+    data: RESTPutAPIChannelPermissionJSONBody,
+    reason?: string,
+  ) {
+    return this.put<RESTPutAPIChannelPermissionResult>(
+      `/channels/${channelID}/permissions/${overwriteID}`,
+      {
+        body: data,
+        reason,
+      },
+    );
+  }
+
+  getChannelInvites(channelID: string) {
+    return this.get<RESTGetAPIChannelInvitesResult>(
+      `/channels/${channelID}/invites`,
     );
   }
 
